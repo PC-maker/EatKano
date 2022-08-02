12fb71c5e31454ee5802bdf260eaaa94a0324c56
PC-maker <110469339+PC-maker@users.noreply.github.com>
2022-08-02 21:49:41 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -39,7 +39,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     $('html').attr('lang', I18N['lang']);
 
     let isDesktop = !navigator['userAgent'].match(/(ipad|iphone|ipod|android|windows phone)/i);
-    let fontunit = isDesktop ? 20 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
+    let fontunit = isDesktop ? 40 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
     document.write('<style type="text/css">' +
         'html,body {font-size:' + (fontunit < 30 ? fontunit : '30') + 'px;}' +
         (isDesktop ? '#welcome,#GameTimeLayer,#GameLayerBG,#GameScoreLayer.SHADE{position: absolute;}' :
@@ -135,7 +135,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
 
     function refreshSize() {
         clearTimeout(refreshSizeTime);
-        refreshSizeTime = setTimeout(_refreshSize, 400);
+        refreshSizeTime = setTimeout(_refreshSize, 200);
     }
 
     function _refreshSize() {
